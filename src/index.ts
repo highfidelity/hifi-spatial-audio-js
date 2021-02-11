b92591be233a69ec419a9396414af0f8cd318fce
Zach Fox <fox@highfidelity.io>
2021-02-11 14:01:51 -0500

    Report HiFiAPIVersion in Web JS via HighFidelityAudio.HiFiAPIVersion

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+declare var HIFI_API_VERSION: string;
+
 import { HiFiAudioAPIData, ReceivedHiFiAudioAPIData, OrientationEuler3D, OrientationQuat3D, Point3D } from "./classes/HiFiAudioAPIData";
 import { HiFiCommunicator, HiFiConnectionStates } from "./classes/HiFiCommunicator";
 import { AvailableUserDataSubscriptionComponents, UserDataSubscription } from "./classes/HiFiUserDataSubscription";
@@ -5,6 +7,11 @@ import { HiFiLogLevel, HiFiLogger } from "./utilities/HiFiLogger";
 import { getBestAudioConstraints, preciseInterval } from "./utilities/HiFiUtilities";
 import { HiFiConstants } from "./constants/HiFiConstants";
 import { HiFiAxes, HiFiHandedness, HiFiAxisConfiguration } from "./classes/HiFiAxisConfiguration";
+
+let isBrowserContext = typeof self !== 'undefined';
+if (isBrowserContext) {
+    exports.HiFiAPIVersion = HIFI_API_VERSION;
+}
 
 exports.HiFiCommunicator = HiFiCommunicator;
 exports.HiFiConnectionStates = HiFiConnectionStates;
