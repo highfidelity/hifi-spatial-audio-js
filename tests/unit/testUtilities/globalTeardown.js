2935f93d6cef73e70983e8c493358683c60194d9
Maia Hansen <maia@highfidelity.com>
2021-03-04 18:42:30 -0800

    No ticket: tests directory is breaking the node build

--- tests/unit/testUtilities/globalTeardown.js
+++
@@ -1,5 +0,0 @@
-// This function is triggered once after all test suites.
-// If we don't run this, some code within wrtc prevents Jest from exiting properly.
-module.exports = () => {
-    process.exit(0)
-};
