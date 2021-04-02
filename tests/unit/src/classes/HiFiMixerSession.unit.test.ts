eb2e11c7a3445f455285389c726a5721cefd2cc3
RebeccaStankus <Rebecca@highfidelity.io>
2021-04-02 15:07:10 -0700

    Test the testing process

--- tests/unit/src/classes/HiFiMixerSession.unit.test.ts
+++ tests/unit/src/classes/HiFiMixerSession.unit.test.ts
@@ -2,5 +2,5 @@ import { HiFiMixerSession } from "../../../../src/classes/HiFiMixerSession";
 
 test(`brand new mixer session can't connect`, () => {
     let newMixerSession = new HiFiMixerSession({});
-    expect(newMixerSession.connectToHiFiMixer({ webRTCSessionParams: {} })).rejects.toBeDefined();
+    expect(newMixerSession.connect({ webRTCSessionParams: {} })).rejects.toBeDefined();
 });
