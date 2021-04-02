946f9fa0de4455fbb41d1e3e86c1c8486a5c651c
RebeccaStankus <Rebecca@highfidelity.io>
2021-04-02 10:08:29 -0700

    More GHA fine tuning

--- tests/unit/src/classes/HiFiMixerSession.unit.test.ts
+++ tests/unit/src/classes/HiFiMixerSession.unit.test.ts
@@ -2,5 +2,5 @@ import { HiFiMixerSession } from "../../../../src/classes/HiFiMixerSession";
 
 test(`brand new mixer session can't connect`, () => {
     let newMixerSession = new HiFiMixerSession({});
-    expect(newMixerSession.connect()).rejects.toBeDefined();
+    expect(newMixerSession.connectToHiFiMixer({ webRTCSessionParams: {} })).rejects.toBeDefined();
 });
