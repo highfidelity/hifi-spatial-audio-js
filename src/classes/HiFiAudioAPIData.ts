bc483c046aef02621820eeaa884f2aa8702c72c8
bridie-hifi <bridie@highfidelity.com>
2021-04-27 13:24:38 -0700

    Updated X character to match other documentaiton!

--- src/classes/HiFiAudioAPIData.ts
+++ src/classes/HiFiAudioAPIData.ts
@@ -399,7 +399,7 @@ export class HiFiAudioAPIData {
      * 
      * ✔ The client sends `hiFiGain` data to the server when `_transmitHiFiAudioAPIDataToServer()` is called.
      * 
-     * x The server does not send `hiFiGain` data to all clients as part of "peer updates".
+     * ❌ The server does not send `hiFiGain` data to all clients as part of "peer updates".
      */
     hiFiGain: number;
     /**
