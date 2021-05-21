e66a3312bac92aa04cd629166a587a7a44c2a675
RebeccaStankus <Rebecca@highfidelity.io>
2021-05-21 15:33:48 -0700

    Add redirection for alternate eu stack names

--- tests/smoke/rest.smoke.test.ts
+++ tests/smoke/rest.smoke.test.ts
@@ -23,6 +23,9 @@ describe('HiFi API REST Calls', () => {
     } else if (stackname === "api-pro-east.highfidelity.com" || stackname === "api-pro-latest-east.highfidelity.com") {
         stackData = stacks.east;
         console.log("_______________USING EAST AUTH FILE_______________________");
+    } else if (stackname === "api-pro-eu.highfidelity.com" || stackname === "api-pro-latest-eu.highfidelity.com") {
+        stackData = stacks['api-pro-eu.highfidelity.com'];
+        console.log("_______________USING EU AUTH FILE_______________________");
     } else if (stackname === "api.highfidelity.com" || stackname === "api-hobby-latest.highfidelity.com") {
         stackData = stacks.hobby;
         console.log("_______________USING HOBBY AUTH FILE_______________________");
