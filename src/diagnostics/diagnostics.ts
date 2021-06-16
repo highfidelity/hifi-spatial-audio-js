d6b54de87b86ad0a29888d18a6ee5544d4252619
howard-stearns <howard@highfidelity.io>
2021-06-15 17:05:55 -0700

    work in browser AND node (but the latter silently not reporting of node-fetch not installed). Also report session end and concurrency=>num_clients

--- src/diagnostics/diagnostics.ts
+++ src/diagnostics/diagnostics.ts
@@ -1,6 +1,22 @@
 import { HiFiMixerSession } from "../classes/HiFiMixerSession";
 import { RaviSession, STATS_WATCHER_FILTER } from "../libravi/RaviSession";
 
+const isBrowser = typeof window !== 'undefined';
+const noop = (_:any):any => undefined;
+const xStorage = isBrowser ? window.localStorage : {getItem: noop, setItem: noop, removeItem: noop};
+const xAddEventListener:any = isBrowser ? window.addEventListener : noop;
+const xRemoveEventListener:any = isBrowser ? window.removeEventListener : noop;
+const xDocument = isBrowser ? window.document : {visibilityState: true, addEventListener: noop, removeEventListener: noop};
+const xNavigator = isBrowser ? window.navigator : {onLine: true, userAgent: `NodeJS ${(process.report.getReport() as any).header.nodejsVersion}`};
+let xfetch = isBrowser && window.fetch;
+if (!isBrowser) {
+    try {
+        xfetch = require('node-fetch');
+    } catch (e) {
+        ; // Remains falsey. Don't report, don't log
+    }
+}
+
 const nonOperative = "non-operative";
 
 STATS_WATCHER_FILTER.set('remote-inbound-rtp',
@@ -23,7 +39,7 @@ let nStatsClients = 0;
 let browserStats: CandidateReport = {};
 let remoteStats: CandidateReport = {};
 let reports:any;
-const useDebugPrefixes = true;
+const useDebugPrefixes = false;
 
 /** 
  * @internal
@@ -46,7 +62,7 @@ export class Diagnostics {
     fireListener: Function;
     onlineListener: Function;
 
-    constructor({url = "http://localhost:3000/disconnect-diagnostics", // FIXME: "https://highfidelity.com/disconnect-diagnostics",
+    constructor({url = "https://webrtc-diag.highfidelity.com/api/v1/logs/post_logs",
                  label, session, ravi, fireOn = []}:{url?:string, label:string, session:HiFiMixerSession, ravi:RaviSession, fireOn?:Array<string>}) {
         Object.assign(this, {url, label, session, ravi, fireOn});
         this.checkPersisted();
@@ -63,7 +79,7 @@ export class Diagnostics {
         this.identifier = identifier;
         this.grabRTCInternals();
         Diagnostics.startStats(this.session);
-        this.fireOn.forEach(event => (document as any).addEventListener(event, this.fireListener));
+        this.fireOn.forEach(event => (xDocument as any).addEventListener(event, this.fireListener));
     }
     /**
      * Call this when we get into a state that we want to know more about, e.g., when leaving the thing that caused us to prime().
@@ -91,7 +107,7 @@ export class Diagnostics {
         this.explicitApplicationClose = false;
         this.webSocket = this.rtc = {};
         // don't leave them hanging around. E.g., beforeunload can mess with the bfcache.
-        this.fireOn.forEach(event => (document as any).removeEventListener(event, this.fireListener));
+        this.fireOn.forEach(event => (xDocument as any).removeEventListener(event, this.fireListener));
         reports = {
             'outbound-rtp': {},
             'inbound-rtp': {},
@@ -106,34 +122,34 @@ export class Diagnostics {
      * Answer a single (long) log line to report.
      */
     toString() {
-        return `${new Date().toISOString()} ${this.identifier}` +
+        return `${new Date().toISOString()} ${this.identifier} applogReason_sessionEND` +
             this.connectionStats('browserStats') +
             this.connectionStats('mixerStats') +
             this.rtpStats() +
             this.rtcStates() +
-            this.s('CONCURRENCY', this.session.concurrency, '\n') +
+            this.s('NUM_CONNECTED', this.session.concurrency, '\n') +
             this.s('APPSTATE', this.session.getCurrentHiFiConnectionState(), '\n') +
             this.s('RAVISTATE', this.ravi.getState()) +
-            this.s('ONLINE', navigator.onLine ? 'yes' : 'no') +
+            this.s('ONLINE', xNavigator.onLine ? 'yes' : 'no') +
             this.s('XPLICITCLOSED', this.explicitApplicationClose ? 'yes' : 'no') +
             this.visibilityInfo() +
             this.connectionInfo() +
             (useDebugPrefixes ? '\n' : '') +
-            ` [${navigator.userAgent}]`;
+            ` [${xNavigator.userAgent}]`;
     }
     s(name:string, value:any, debugPrefix = '') {
         let separator = isNaN(value) ? '_' : ':';
         return `${useDebugPrefixes ? debugPrefix : ''} ${this.label}${name}${separator}${value}`;
     }
     connectionInfo() {
-        const info:any = (navigator as any).connection || (navigator as any).mozConnection || (navigator as any).webkitConnection || {};
+        const info:any = (xNavigator as any).connection || (xNavigator as any).mozConnection || (xNavigator as any).webkitConnection || {};
         return this.s('DEVICE', info.type, '\n') +
             this.s('RATING', info.effectiveType) +
             this.s('DL', info.downlink) +
             this.s('RTT', info.rtt);
     }
     visibilityInfo() {
-        return this.s('VISIBLITY', document.visibilityState);
+        return this.s('VISIBLITY', xDocument.visibilityState);
     }
     /**
      * When we fire, I'd like to directly ask the RTCPeerConnection and our signaling WebSocket some questions, as a sanity check
@@ -190,42 +206,42 @@ export class Diagnostics {
      * Return success, or a promise for success.
      */
     report(reportString:string) {
-        if (!navigator.onLine) return false;
-        console.warn(reportString); // FIXME: remove after testing.
+        if (!xNavigator.onLine) return false;
+        if (!xfetch) return false;
         // This will return true if the beacon is successfully queued, not sent.
         // Ultimately, we would need a verification (e.g., next session) to see if the previous identifier was logged.
         // A failure gets logged to console in some browsers, but they don't actually emit an error event.
         // return navigator.sendBeacon(this.url, reportString);
         // Instead, let's POST exactly as sendBeacon would, and check success:
-        return fetch(this.url, {
+        return xfetch(this.url, {
             method: 'POST',
             headers: { 'Content-Type': 'text/plain' },
             body: reportString
-        }).then((response:Response) => response.ok, () => false);
+        }).then((response:Response) => response.ok, (x:any) => false);
     }
     /**
      * Add reportString to the set of data being saved for later reporting.
      */
     persist(reportString:string) {
-        let existing = localStorage.getItem(this.label) || "";
+        let existing = xStorage.getItem(this.label) || "";
         // By construction existing is expected to be empty or one line. It could have multiple lines if
         // there is a bug, or if the application site limits the connect-src (or default-src)
         // in its Content-Security-Policy header without allowing this.url.
         // If it is more than a line, we are accumulating stuff and really ought to phone home through the mixer when connected.
         if (existing) existing += "\n";
-        window.localStorage.setItem(this.label, existing + reportString);
+        xStorage.setItem(this.label, existing + reportString);
         // An optimization to get caught up on data quicker in the case where network is lost and returns while tab is still up.
-        (window as any).addEventListener('online', this.onlineListener);
+        xAddEventListener('online', this.onlineListener);
     }
     /**
      * If there's anything persisted, try to report it. If successful, clear persistence.
      */
     async checkPersisted() {
-        (window as any).removeEventListener('online', this.onlineListener);
-        let existing = localStorage.getItem(this.label);
+        xRemoveEventListener('online', this.onlineListener);
+        let existing = xStorage.getItem(this.label);
         if (!existing) return;
         if (! await this.report(existing)) return;
-        window.localStorage.removeItem(this.label);
+        xStorage.removeItem(this.label);
     }
     // RTC stats...
     // Ravi only allows one stats collection function at a time, so we'll have to share
