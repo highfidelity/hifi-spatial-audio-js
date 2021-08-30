c33c06fd075518d719d452443be79d72e6b003dd
sabrina-shanman <sabrina@highfidelity.io>
2021-08-30 11:39:05 -0700

    In index.ts, export public API types in HiFiMixerSession.ts

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,7 @@ export { apiVersion };
 // Scroll down to check out those shorter synonyms.
 export { HiFiAudioAPIData, ReceivedHiFiAudioAPIData, OrientationEuler3D, OrientationQuat3D, Point3D, eulerToQuaternion, eulerFromQuaternion} from "./classes/HiFiAudioAPIData";
 export { HiFiCommunicator, HiFiConnectionStates, HiFiUserDataStreamingScopes } from "./classes/HiFiCommunicator";
+export { SetOtherUserGainForThisConnectionResponse, SetOtherUserGainsForThisConnectionResponse, MuteReason, MuteChangedEvent, OnMuteChangedCallback } from "./classes/HiFiMixerSession";
 export { WebRTCSessionParams } from "./libravi/RaviSession";
 export { AvailableUserDataSubscriptionComponents, UserDataSubscription } from "./classes/HiFiUserDataSubscription";
 export { HiFiLogLevel, HiFiLogger } from "./utilities/HiFiLogger";
